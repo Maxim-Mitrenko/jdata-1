cd6371c5ed1f711522a4036528742297f738f267
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-09-14 21:26:06 +0300

    Initial commit

---
+++ add.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES ('Иван', 'Иванов', 25, '+79999992123', 'MOSCOW');
+INSERT INTO users VALUES('Петр', 'Сергеев', 29, '+79999992123', 'VOLGOGRAD');
+INSERT INTO users VALUES ('Иван', 'Петров', 31, '+79999992123', 'MOSCOW');
+INSERT INTO users VALUES ('Лев', 'Андреев', 35, '+79999992123', 'MOSCOW');
---
+++ age.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users
+WHERE age > 27
+ORDER BY age DESC
